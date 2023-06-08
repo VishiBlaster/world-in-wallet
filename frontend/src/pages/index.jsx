b0cb3a1b0ae8408d1d616ceb3bbd4d7b7897673d
VishiBlaster <mongajatin2588@gmail.com>
2023-06-08 18:42:35 +0530

    a backup

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import navbar from '../components/navbar';
-import InfoScheme from '../components/InfoScheme';
 
 const index = () => {
 	return (
 		<>
 			<navbar />
-			<InfoScheme />
 		</>
 	);
 };
--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,34 +1,26 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  Redirect,
+  Routes,
+  Navigate
 } from "react-router-dom";
-import Header from "../components/Header/Header";
-import CarHireScreen from "../pages/CarHireScreen";
-import FlightsScreen from "../pages/FlightsScreen";
-import HotelsScreen from "../pages/HotelsScreen";
+import navbar from "../components/navbar";
 import index from "../pages/index";
-import ResultsScreen from "../pages/ResultsScreen";
 
 const AppRouter = () => {
   return (
     <>
       <Router>
-        <Header />
-        <Switch>
+        <navbar />
+        <Routes>
           <Route exact path="/" component={index} />
-          <Route exact path="/flights" component={FlightsScreen} />
-          <Route exact path="/hotels" component={HotelsScreen} />
-          <Route exact path="/carhire" component={CarHireScreen} />
           <Route
             exact
             path="/transport/flights/:fromId/:fromName/:toId/:toName/:depart/:returnDate/"
-            component={ResultsScreen}
           />
-          <Redirect to="/" />
-        </Switch>
+          <Navigate to="/" />
+        </Routes>
       </Router>
     </>
   );
